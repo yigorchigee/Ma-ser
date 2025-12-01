a81d629c5a59515d3e5b16bf5166a8c56ff202c6
yigorchigee <zevitita99@gmail.com>
2025-12-01 14:57:48 -0800

    Use uploaded transparent logo asset

--- src/components/icons/TzedakaLogo.jsx
+++ src/components/icons/TzedakaLogo.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function TzedakaLogo({ className = '' }) {
   return (
-    <img
-      src="/Tzedaka Tracker with text to the right.png"
-      alt="Tzedaka Tracker logo"
-      className={className}
-      loading="lazy"
-    />
+    <div className={`relative inline-block overflow-hidden rounded-2xl ${className}`}>
+      <img
+        src="/Tzedaka%20Tracker%20with%20text%20to%20the%20right.png"
+        alt="Tzedaka Tracker logo"
+        className="h-full w-full object-contain"
+        loading="lazy"
+      />
+    </div>
   );
 }
