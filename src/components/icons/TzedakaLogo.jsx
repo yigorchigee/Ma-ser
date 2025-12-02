1fc385ffc9cb090a8bc006074cf793313bf810c0
yigorchigee <zevitita99@gmail.com>
2025-12-01 18:44:28 -0800

    Adjust taskbar logo position

--- src/components/icons/TzedakaLogo.jsx
+++ src/components/icons/TzedakaLogo.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function TzedakaLogo({ className = '' }) {
   return (
-    <div className={`relative inline-block overflow-hidden rounded-2xl bg-white ${className}`}>
+    <div className={`relative inline-block overflow-hidden rounded-2xl ${className}`}>
       <img
-        src="/Tzedaka Tracker with text to the right.png"
+        src="/tzedaka-logo.svg"
         alt="Tzedaka Tracker logo"
         className="h-full w-full object-cover object-top"
         loading="lazy"
