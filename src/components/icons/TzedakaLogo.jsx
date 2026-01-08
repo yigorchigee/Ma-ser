a0823792d98c5078f0fa3d209899df1e20c11479
YosefENav <yosefn555@gmail.com>
2026-01-08 10:08:49 -0800

    Update TzedakaLogo to use imported logo image
    Fixed the image import to properly use ES6 import statement instead of hardcoded path. This ensures the image loads correctly in React/React Native.

--- src/components/icons/TzedakaLogo.jsx
+++ src/components/icons/TzedakaLogo.jsx
@@ -1,10 +1,11 @@
+import logo from '../../../image.png';
 import React from 'react';
 
 export default function TzedakaLogo({ className = '' }) {
   return (
-    <img 
-      src="/image.png" 
-      alt="Tzedaka Tracker Logo" 
+    <img
+      src={logo}
+      alt="Tzedaka Tracker Logo"
       className={className}
     />
   );
