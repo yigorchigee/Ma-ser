773a4c52200eda17821ce68d52afccd227b81b98
yigorchigee <zevitita99@gmail.com>
2026-02-26 18:28:30 -0800

    Delete src/components/icons/TzedakaLogo.jsx

--- src/components/icons/TzedakaLogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TzedakaLogo({ className = '' }) {
-  return (
-    <div className={`inline-flex items-center justify-center ${className}`} role="img" aria-label="Tzedaka Tracker logo">
-      <img src="/tzedaka-tracker-logo.png" alt="Tzedaka Tracker" className="h-full w-full object-contain" />
-    </div>
-  );
-}
